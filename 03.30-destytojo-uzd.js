11fbaeda71fe7cb3ead9231db9f1fbb9db8f8a71
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-03-30 19:10:32 +0300

    for ciklas done

---
+++ 03.30-destytojo-uzd.js
@@ -0,0 +1,145 @@
+// Ciklo for panaudojimas 
+
+// 1 a
+console.log('---1a---');
+let suma = 0;
+
+for (let i = 0; i < 1; i++) {
+    suma += i;
+    console.log(suma);
+}
+
+// 1 b
+console.log('---1b---');
+
+suma = 0;
+for (let i = 0; i <= 4; i++) {
+   suma += i;
+}
+console.log(suma);
+
+// 1 c
+console.log('---1c---');
+
+suma = 0;
+for (let i = 0; i <= 100; i++) {
+    suma += i;
+}
+console.log(suma);
+
+// 1 d
+console.log('---1d---');
+
+suma = 0;
+for (let i = 574; i <= 815; i++) {
+    suma += i;
+}
+console.log(suma);
+
+// 1 e
+console.log('---1e---');
+
+suma = 0;
+for (let i = -50; i <= 50; i++) {
+    suma += i;
+}
+console.log(suma);
+
+// 1 f
+console.log('---1f---');
+
+suma = 0;
+for (let i = -70; i <= 30; i++) {
+    suma += i;
+}
+console.log(suma);
+
+// 2
+console.log('---2---');
+
+const text = 'abcdef';
+const splitText = text.split('');
+const reverseText = splitText.reverse();
+const result = reverseText.join('');
+console.log(result);
+
+// 3a
+console.log('---3a---');
+
+let atsakymas = 0;
+for (let i = 0; i <= 11; i++) {
+    if (i % 3 === 0) {
+        atsakymas++;
+    }
+} 
+console.log(`Skaiciu intervale tarp 0 ir 11, besidalijanciu be liekanos is 3 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = 0; i <= 11; i++) {
+    if (i % 5 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp 0 ir 11, besidalijanciu be liekanos is 5 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = 0; i <= 11; i++) {
+    if (i % 7 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp 0 ir 11, besidalijanciu be liekanos is 7 yra ${atsakymas} vienetai.`);
+
+// 3 b
+console.log('---3b---');
+
+atsakymas = 0;
+for (let i = 8; i <= 31; i++) {
+    if (i % 3 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp 8 ir 31, besidalijanciu be liekanos is 3 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = 8; i <= 31; i++) {
+    if (i % 5 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp 8 ir 31, besidalijanciu be liekanos is 5 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = 8; i <= 31; i++) {
+    if (i % 7 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp 8 ir 31, besidalijanciu be liekanos is 7 yra ${atsakymas} vienetai.`);
+
+// 3 c
+console.log('---3c---');
+
+atsakymas = 0;
+for (let i = -18; i <= 18; i++) {
+    if (i % 3 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp -18 ir 18, besidalijanciu be liekanos is 3 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = -18; i <= 18; i++) {
+    if (i % 5 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp -18 ir 18, besidalijanciu be liekanos is 3 yra ${atsakymas} vienetai.`);
+
+atsakymas = 0;
+for (let i = -18; i <= 18; i++) {
+    if (i % 7 === 0) {
+        atsakymas++;
+    }
+}  
+console.log(`Skaiciu intervale tarp -18 ir 18, besidalijanciu be liekanos is 3 yra ${atsakymas} vienetai.`);
