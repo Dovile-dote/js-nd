3aad249b4e77dd6b7404fb6fb5be5842711362c1
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-03-30 17:36:20 +0300

    Papildomu uzduociu sprendimai

--- 03.30.js
+++ 03.30.js
@@ -55,3 +55,60 @@
 // console.log(`Saulius gali nusipirkti ${k} porcijas ledu ir jam liks ${m} centu grazos.`)
 
 // 6
+
+// let n = 6;
+
+// if (n % 2 === 0) {
+//     console.log('Trikampi sudelioti galima.');
+// } else {
+//     console.log('Trikampio sudelioti negalima.');
+// }
+
+// 7
+
+// let n = 2;
+// if (n === 2) {
+//     d = 28;
+// } else if (n % 2 === 0) {
+//     d = 30;
+// } else {
+//     d = 31;
+// }
+// console.log('Sis menuo turi ' + d + ' dienu.');
+
+// 8
+
+// let k = 4;
+// if (k % 2 === 0) {
+//     console.log('Kambari tvarkys vyresnelis.')
+// } else {
+//     console.log('Kambari tvarkys jaunelis.')
+// }
+
+// 9
+
+let m = 1904;
+
+console.log(m)
+
+if ((m - 1896) % 4 === 0) {
+    n = 1 + ((m - 1896) / 4);
+    console.log(`${m} metais vyko ${n}-os rungtynes.`)
+} else {
+    console.log(`Metai neolimpiniai.`)
+}
+
+
+// 10
+
+v1 = 8;
+m1 = 29;
+m2 = 23;
+v = 9;
+m = 5;
+
+if (v1 * 60 + m1 + m2 < v * 60 + 5) {
+    console.log('Petras i mokykla neveluos.');
+} else {
+    console.log('Petras i mokykla paveluos.');
+}
