8f9dc91a96443ad6844a23cdf5b812618b087c4f
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-03-30 19:46:09 +0300

    no more comments

--- 03.30.js
+++ 03.30.js
@@ -2,92 +2,92 @@
 
 // 1
 
-// const n = 20;
-// if (n < 0) {
-//     console.log('Blogas');
-// } else {
-//     console.log('Geras');
-// }
+let n = 20;
+if (n < 0) {
+    console.log('Blogas');
+} else {
+    console.log('Geras');
+}
 
 // 2
 
-// const n = 2;
-// if (n === 1) {
-//     console.log('Galite eiti');
-// } else if (n === 2) {
-//     console.log('Palaukite');
-// } else {
-//     console.log('Eiti draudziama!');
-// }
+n = 2;
+if (n === 1) {
+    console.log('Galite eiti');
+} else if (n === 2) {
+    console.log('Palaukite');
+} else {
+    console.log('Eiti draudziama!');
+}
 
 // 3
 
-// const d = 3;
-// const k = 18;
-// const n = 5;
+let d = 3;
+let k = 18;
+n = 5;
 
-// if (k/n < d) {
-//     console.log('Knygos telpa i dezes');
-// } else {
-//     console.log('Knygos netelpa i dezes');
-// }
+if (k/n < d) {
+    console.log('Knygos telpa i dezes');
+} else {
+    console.log('Knygos netelpa i dezes');
+}
 
 // 4
 
-// let a = 100;
-// let b = 9;
+let a = 100;
+let b = 9;
 
-// if (a > b) {
-//     console.log(--a);
-//     console.log(++b);
-// } else {
-//     console.log(++a);
-//     console.log(--b);
-// }
+if (a > b) {
+    console.log(--a);
+    console.log(++b);
+} else {
+    console.log(++a);
+    console.log(--b);
+}
 
 // 5
 
-// let c = 50;
-// let p = 20;
-// let k = Math.floor(c / p);
-// let m = c - p * k;
-// console.log('Saulius gali nusipirkti ' + k + ' porcijas ledu ir jam liks ' + m + ' centu.');
-// console.log(`Saulius gali nusipirkti ${k} porcijas ledu ir jam liks ${m} centu grazos.`)
+let c = 50;
+let p = 20;
+k = Math.floor(c / p);
+m = c - p * k;
+console.log('Saulius gali nusipirkti ' + k + ' porcijas ledu ir jam liks ' + m + ' centu.');
+console.log(`Saulius gali nusipirkti ${k} porcijas ledu ir jam liks ${m} centu grazos.`)
 
 // 6
 
-// let n = 6;
+n = 6;
 
-// if (n % 2 === 0) {
-//     console.log('Trikampi sudelioti galima.');
-// } else {
-//     console.log('Trikampio sudelioti negalima.');
-// }
+if (n % 2 === 0) {
+    console.log('Trikampi sudelioti galima.');
+} else {
+    console.log('Trikampio sudelioti negalima.');
+}
 
 // 7
 
-// let n = 2;
-// if (n === 2) {
-//     d = 28;
-// } else if (n % 2 === 0) {
-//     d = 30;
-// } else {
-//     d = 31;
-// }
-// console.log('Sis menuo turi ' + d + ' dienu.');
+n = 2;
+if (n === 2) {
+    d = 28;
+} else if (n % 2 === 0) {
+    d = 30;
+} else {
+    d = 31;
+}
+console.log('Sis menuo turi ' + d + ' dienu.');
 
 // 8
 
-// let k = 4;
-// if (k % 2 === 0) {
-//     console.log('Kambari tvarkys vyresnelis.')
-// } else {
-//     console.log('Kambari tvarkys jaunelis.')
-// }
+k = 4;
+if (k % 2 === 0) {
+    console.log('Kambari tvarkys vyresnelis.')
+} else {
+    console.log('Kambari tvarkys jaunelis.')
+}
 
 // 9
 
-let m = 1904;
+m = 1904;
 
 console.log(m)
 
@@ -107,7 +107,7 @@ m2 = 23;
 v = 9;
 m = 5;
 
-if (v1 * 60 + m1 + m2 < v * 60 + 5) {
+if (v1 * 60 + m1 + m2 < v * 60 + m) {
     console.log('Petras i mokykla neveluos.');
 } else {
     console.log('Petras i mokykla paveluos.');
