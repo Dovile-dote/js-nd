caede6ea4be001c2d7f0c660f89a725394526c32
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-03-30 16:00:52 +0300

    pimos penkios uzduotys

---
+++ 03.30.js
@@ -0,0 +1,57 @@
+// Papildomi uzdaviniai
+
+// 1
+
+// const n = 20;
+// if (n < 0) {
+//     console.log('Blogas');
+// } else {
+//     console.log('Geras');
+// }
+
+// 2
+
+// const n = 2;
+// if (n === 1) {
+//     console.log('Galite eiti');
+// } else if (n === 2) {
+//     console.log('Palaukite');
+// } else {
+//     console.log('Eiti draudziama!');
+// }
+
+// 3
+
+// const d = 3;
+// const k = 18;
+// const n = 5;
+
+// if (k/n < d) {
+//     console.log('Knygos telpa i dezes');
+// } else {
+//     console.log('Knygos netelpa i dezes');
+// }
+
+// 4
+
+// let a = 100;
+// let b = 9;
+
+// if (a > b) {
+//     console.log(--a);
+//     console.log(++b);
+// } else {
+//     console.log(++a);
+//     console.log(--b);
+// }
+
+// 5
+
+// let c = 50;
+// let p = 20;
+// let k = Math.floor(c / p);
+// let m = c - p * k;
+// console.log('Saulius gali nusipirkti ' + k + ' porcijas ledu ir jam liks ' + m + ' centu.');
+// console.log(`Saulius gali nusipirkti ${k} porcijas ledu ir jam liks ${m} centu grazos.`)
+
+// 6
