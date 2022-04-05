86dfcfac45ec04d54cf0c03617768c28de53f793
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-04-05 12:26:40 +0300

    Extra test

--- 04.01-destytojo-uzd.js
+++ 04.01-destytojo-uzd.js
@@ -48,67 +48,123 @@ console.log(dauginimas('labas', 5));
 console.log(dauginimas(7.7, ''));
 console.log(dauginimas('', 5));
 
-// console.log('-----3-----');
+console.log('-----3-----');
 
-// function skaitmenuKiekisSkaiciuje(n) {
-//     if (isNaN(n) || n === true || n === false){
-//         return `Pateikta netinkamo tipo reiksme`;
-//     } else {
-//         let string = n.toString()
-//         const skaitmenuKiekis = string.length;
-//         return `${n} skaiciuje yra ${skaitmenuKiekis} skaitmenu.`;
-//     }
-// }
-// console.log(skaitmenuKiekisSkaiciuje(5));
-// console.log(skaitmenuKiekisSkaiciuje(781));
-// console.log(skaitmenuKiekisSkaiciuje(37060123456));
-// console.log(skaitmenuKiekisSkaiciuje(true));
-// console.log(skaitmenuKiekisSkaiciuje("asd"));
-// console.log(skaitmenuKiekisSkaiciuje(NaN));
+function skaitmenuKiekisSkaiciuje(n) {
+    if (typeof n !== 'number' || !isFinite(n)){
+        return `Pateikta netinkamo tipo reiksme`;
+    } else {
+        const string = '' + n;
+        let skaitmenuKiekis = string.length;
+        // if (string.includes('.')) 
+        if (n % 1 !== 0) {
+            skaitmenuKiekis --;
+        }
+
+        if (n < 0) {
+            skaitmenuKiekis --;
+        }        
+        return `${n} skaiciuje yra ${skaitmenuKiekis} skaitmenu.`;
+    }
+}
+console.log(skaitmenuKiekisSkaiciuje(5));
+console.log(skaitmenuKiekisSkaiciuje(781));
+console.log(skaitmenuKiekisSkaiciuje(37060123456));
+console.log(skaitmenuKiekisSkaiciuje(true));
+console.log(skaitmenuKiekisSkaiciuje("asd"));
+console.log(skaitmenuKiekisSkaiciuje(NaN));
 
 
-// console.log('-----4-----');
+console.log('-----4-----');
 
-// function didziausiaisSkaiciusSarase(sarasas) {
-//     if (Array.isArray(sarasas) === false) {
-//         return `Pateikta netinkamo tipo reiksme`;
-//     } else if (sarasas.length === 0) {
-//         return `Pateiktas sarasas negali buti tuscias.`;
-//     } else {
-//         let didziausias = sarasas[0];
-//         for (let i = 0; i < sarasas.length; i++) {
-//             if (didziausias < sarasas[i]) {
-//                 didziausias = sarasas[i];
-//             }
-//         }
-//         return `Didziausias skaicius: ${didziausias}`
-//     }
-// }
+function didziausiaisSkaiciusSarase(sarasas) {
+    if (Array.isArray(sarasas) === false) {
+        return `Pateikta netinkamo tipo reiksme`;
+    } else if (sarasas.length === 0) {
+        return `Pateiktas sarasas negali buti tuscias.`;
+    } else {
+        let didziausias = sarasas[0];
+        for (let i = 0; i < sarasas.length; i++) {
+            if (didziausias < sarasas[i]) {
+                didziausias = sarasas[i];
+            }
+        }
+        return `Didziausias skaicius: ${didziausias}`
+    }
+}
 
-// let x = [1];
-// let atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+let x = [1];
+let atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = [1, 2, 3];
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = [1, 2, 3];
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = [-5, 78, 14, 0, 18];
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = [-5, 78, 14, 0, 18];
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = [69, 69, 69, 69, 66];
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = [69, 69, 69, 69, 66];
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = [-1, -2, -3, -4, -5, -6, -7, -8];
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = [-1, -2, -3, -4, -5, -6, -7, -8];
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = "pomidoras";
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = "pomidoras";
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
 
-// x = [];
-// atsakymas = didziausiaisSkaiciusSarase(x);
-// console.log(atsakymas);
+x = [];
+atsakymas = didziausiaisSkaiciusSarase(x);
+console.log(atsakymas);
+
+
+function bigNum(list) {
+    // VALIDATION
+    if (typeof list !== 'object') {
+        return `ERROR: netinkamas tipas`;
+    } 
+
+    if (list.length === 0) {
+        return `ERROR: sarasas negali buti tuscias`;
+    }
+    
+    // LOGIC
+    let biggestNumber = -Infinity;
+
+    for (let i = 0; i < list.length; i++) {
+        const number = list[i];
+        // INNER VALIDATION
+        if (typeof number !== 'number' || !isFinite(number)) {
+            continue;
+        }
+        // INNER LOGIC
+        if (number > biggestNumber) {
+            biggestNumber = number;
+        }
+    }
+
+    //  RESULT RETURN
+    if(biggestNumber === - Infinity) {
+        return `ERROR: sarase nera nei vieno normalaus skaiciaus`
+    }
+    return biggestNumber;
+}
+
+console.log(bigNum('pomidoras'), '-->', 'ERROR');
+console.log(bigNum([1]), '-->', 1);
+console.log(bigNum([]), '-->', 'ERROR');
+console.log(bigNum([1, 2, 3]), '-->', 3);
+console.log(bigNum([1, 3, 2]), '-->', 3);
+console.log(bigNum([-5, 78, 14, 0, 18]), '-->', 78);
+console.log(bigNum([69, 69, 69, 69, 66]), '-->', 69);
+console.log(bigNum([-1, -2, -3, -4, -5, -6, -7, -8]), '-->', -1);
+console.log(bigNum([1, 2, 3, 'x']), '-->', 3);
+console.log(bigNum([1, 2, 'x', 3]), '-->', 3);
+console.log(bigNum([1, 'x', 2, 3]), '-->', 3);
+console.log(bigNum(['x', 1, 2, 3]), '-->', 3);
+console.log(bigNum([x]), '-->', 'ERROR');
+console.log(bigNum(['x', true, [], -Infinity, NaN, Infinity]), '-->', 'ERROR');
